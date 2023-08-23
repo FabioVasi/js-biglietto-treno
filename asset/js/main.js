b06031445c6799fc1e8d3616b6c6eeb1e38e0786
FabioVasi <vasifabio96@gmail.com>
2023-08-23 14:14:26 +0200

    Add pseuco-code and instruments

---
+++ asset/js/main.js
@@ -0,0 +1,31 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km).
+va applicato uno sconto del 20% per i minorenni.
+va applicato uno sconto del 40% per gli over 65.
+
+Strumenti.
+
+- prompt
+- console log
+- number
+- condition
+- Math.random
+- Math.floor
+- Math.ceil
+- const
+- let
+- if
+- else
+- document.getElementById
+
+*/
+
+// chiedere numero di chilometri da percorrere
+// chiedere l'età dell'utente
+// calcolare il prezzo del viaggio
+// applicare il 20% di sconto per i minorenni
+// applicare il 40% di sconto per gli over 65
+// stampare il messaggio con il prezzo in console includendo anche due cifre decimali
+// stampare il messaggio con il prezzo in pagina includendo anche due cifre decimali
